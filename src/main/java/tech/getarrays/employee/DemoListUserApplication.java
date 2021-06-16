6aba44dd616f4c6ce43e3278ebcb13792ca3afb0
Larry Caichihua <lcaichihua@hotmail.com>
2021-06-16 01:57:51 -0500

    second coomit

---
+++ src/main/java/tech/getarrays/employee/DemoListUserApplication.java
@@ -0,0 +1,13 @@
+package tech.getarrays.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoListUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoListUserApplication.class, args);
+	}
+
+}
